af30cfa368616953256a3cb0eaf0fd363ede606d
Anders Sveen <anders@f12.no>
2024-07-22 12:35:28 +0200

    Doing a wait

--- react/script.js
+++ react/script.js
@@ -1,6 +1,16 @@
 
+function wait(ms){
+    const start = new Date().getTime();
+    let end = start;
+    while(end < start + ms) {
+        end = new Date().getTime();
+    }
+}
+
+
 // Define a component called Greetings
 function Greetings() {
+    wait(3000);
     return <div>
         <h1>Hello readers, Thank you for reading this blog !</h1>
         <p>{new Date().toLocaleString()}</p>
